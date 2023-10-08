c3ff1e573fedca10845c9fe802090283bc57cbe5
Marko <markolitovchenko24@gmail.com>
2023-10-08 11:36:52 -0400

    Initialized spring project

---
+++ src/main/java/com/markarko/migratify/MigratifyApplication.java
@@ -0,0 +1,13 @@
+package com.markarko.migratify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigratifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigratifyApplication.class, args);
+	}
+
+}
