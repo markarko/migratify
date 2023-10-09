5d3d2361615dbdb3efccd18bdf091f1bd5bdafc4
Marko <markolitovchenko24@gmail.com>
2023-10-08 22:26:16 -0400

    Added method to create a new playlist

--- src/main/java/com/markarko/migratify/services/PlaylistService.java
+++ src/main/java/com/markarko/migratify/services/PlaylistService.java
@@ -3,16 +3,17 @@ package com.markarko.migratify.services;
 import com.markarko.migratify.entities.Playlist;
 import com.markarko.migratify.entities.Playlists;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class PlaylistService {
@@ -25,11 +26,9 @@ public class PlaylistService {
 
     public List<Playlist> getPlaylists(String accessToken) throws HttpClientErrorException {
         String userId = env.getProperty("user_id");
-        String url = "https://api.spotify.com/v1/users/" + userId + "/playlists";
+        String url = String.format("https://api.spotify.com/v1/users/%s/playlists", userId);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/x-www-form-urlencoded");
-        headers.add("Authorization", "Bearer " + accessToken);
+        HttpHeaders headers = buildHeaders(accessToken);
 
         HttpEntity<String> request = new HttpEntity<>(headers);
         RestTemplate restTemplate = new RestTemplate();
@@ -39,7 +38,35 @@ public class PlaylistService {
             return response.getBody().getPlaylists();
         }
 
-        // An HttpClientErrorException will be thrown if the playlists couldn't be fetched
         return new ArrayList<>();
     }
+
+    public ResponseEntity<String> postPlaylist(String accessToken, String playlistName) throws HttpClientErrorException {
+        String userId = env.getProperty("user_id");
+        String url = String.format("https://api.spotify.com/v1/users/%s/playlists", userId);
+
+        HttpHeaders headers = buildHeaders(accessToken);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+        form.add("name", playlistName);
+
+        Map<String, String> data = new HashMap<>();
+        data.put("name", playlistName);
+        data.put("public", "false");
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(data, headers);
+        RestTemplate restTemplate = new RestTemplate();
+
+        return restTemplate.exchange(url, HttpMethod.POST, request, String.class);
+    }
+
+    private HttpHeaders buildHeaders(String accessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/x-www-form-urlencoded");
+        headers.add("Authorization", "Bearer " + accessToken);
+
+        return headers;
+    }
+
 }
