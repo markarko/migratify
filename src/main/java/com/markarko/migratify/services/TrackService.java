6c4dce5733aafe9fe2623d8aed39c41ac5cb7e72
Marko <markolitovchenko24@gmail.com>
2023-10-09 12:48:22 -0400

    Added method to put tracks into a playlist

--- src/main/java/com/markarko/migratify/services/TrackService.java
+++ src/main/java/com/markarko/migratify/services/TrackService.java
@@ -2,15 +2,14 @@ package com.markarko.migratify.services;
 
 import com.markarko.migratify.entities.Track;
 import com.markarko.migratify.entities.Tracks;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class TrackService {
@@ -29,6 +28,19 @@ public class TrackService {
         return new ArrayList<>();
     }
 
+    public ResponseEntity<String> postTracks(String accessToken, String url, List<String> trackUris) {
+        HttpHeaders headers = buildHeaders(accessToken);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("uris", trackUris);
+        data.put("position", 0);
+
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(data, headers);
+        RestTemplate restTemplate = new RestTemplate();
+
+        return restTemplate.exchange(url, HttpMethod.POST, request, String.class);
+    }
     private HttpHeaders buildHeaders(String accessToken) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/x-www-form-urlencoded");
